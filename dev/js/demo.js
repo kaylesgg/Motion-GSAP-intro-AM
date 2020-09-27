7560c37b56ba96350b95dca0aff1017486e0e9b2
kaylesgg <63324097+kaylesgg@users.noreply.github.com>
2020-09-27 16:25:06 -0400

    green

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,38 @@
 import {gsap} from "gsap";
 
-gsap.to(".triangle-red",{duration: 2, alpha:0.5, rotation:180, x:500, y:200})
-gsap.to(".triangle-blue",{duration: 2, alpha:0.5, rotation:90, x:500})
+gsap.to (".dark-green",
+            {duration: 2, rotation: 360, x: 100, borderRadius:"50px"
+            }
+        );
+gsap.to (".med-green",
+            {duration: 2.5, rotation: 360, x: 300, borderRadius:"100px"
+            }
+        );
+gsap.to (".light-green",
+            {duration: 3, rotation: 360, x: 500, borderRadius:"200px"
+            }
+        );
+gsap.to (".light-green",
+            {duration: 1, delay: 2.5, y:-350,borderRadius:"200px"
+            }
+        );
+gsap.to (".med-green",
+            {duration: 1.5, delay: 3, y:-180,borderRadius:"200px"
+            }
+        );
+gsap.to (".dark-green",
+            {duration: 2, delay: 3.5, y:-10,borderRadius:"200px"
+            }
+        );
+gsap.to (".dark-green",
+            {duration: 2, delay: 4.5,x:-10,borderRadius:"200px"
+            }
+        );
+gsap.to (".med-green",
+            {duration: 2, delay: 4.5, x:-10, y:-300, borderRadius:"200px"
+            }
+        );
+gsap.to (".light-green",
+            {duration: 2, delay: 4.5, x:-10, y:-590, borderRadius:"200px"
+            }
+        );
