45089f3b8a2a101be030351f34ccdc79b543f7fa
kaylesgg <63324097+kaylesgg@users.noreply.github.com>
2020-09-27 14:26:47 -0400

    gsap set up

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,11 +1,1 @@
-import {gsap} from "gsap";
-
-
-/* -----------------
-    Demo Script
------------------ */
-
-var timeOfAnimation = 3;
-
-gsap.to(".red-box",{duration: timeOfAnimation,borderRadius: "20"});
-gsap.to(".red-box",{duration: timeOfAnimation, rotation:360, repeat:3, delay:timeOfAnimation });
+console.log("demo");
